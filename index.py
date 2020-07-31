3a46dc7b56f693dbc6f38369b527b0a2b6332efa
Slendercoder <andradeed78@gmail.com>
2020-07-31 17:03:24 -0500

    Working webpage

--- index.py
+++ index.py
@@ -147,5 +147,5 @@ hmc.register_callbacks(app)
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    #app.run_server(host="0.0.0.0", port=8080)
+    #app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8080)
